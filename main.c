48c27e583acfd080bd4eaa9a8a26053de2862b2f
RobinSalihAxelsson <axelsson.robin@outlook.com>
2023-05-20 13:41:15 +0200

    refactor: use self pointer

--- main.c
+++ main.c
@@ -9,76 +9,32 @@ typedef struct {
 typedef struct {
     Contact *contacts;
     int size;
-} DynAddressBook;
+} AddressBook;
 
+AddressBook AddContact(AddressBook* self, Contact contact){
+    self->size++;
+    Contact *ptr = realloc(self->contacts, sizeof(Contact) * self->size);
+    self->contacts = ptr;
+    self->contacts[self->size - 1] = contact;
+}
 
-DynAddressBook DynAddContact(DynAddressBook* book, Contact contact){
-
-    book->size++;
-    Contact *ptr = realloc(book->contacts, sizeof(Contact) * book->size);
-
-    printf("malloc.ptr %p\n", (void*)ptr);
-
-    book->contacts = ptr;
-
-    printf("book.contacts %p\n", (void*)book->contacts);
-
-    book->contacts[book->size - 1] = contact;
+AddressBook* AddressBook_init(){
+    AddressBook *self = malloc(sizeof(AddressBook));
+    self->contacts = NULL;
+    self->size = 0;
+    return self;
 }
 
 int main(){
-    DynAddressBook book;
-    book.contacts = NULL;
-    printf("book.contacts %p\n", (void*)book.contacts);
-
+    AddressBook *book = AddressBook_init();
     Contact contact = {"Robin", "0723079232"};
-    DynAddContact(&book, contact);
+    AddContact(book, contact);
     Contact contact2 = {"Shan", "0723079333"};
-    DynAddContact(&book, contact2);
+    AddContact(book, contact2);
 
-    for (int i = 0; i < book.size; ++i) {
-        printf("name %s, number %s, ptr: %p\n", book.contacts[i].name, book.contacts[i].number, (void*)&book.contacts[i]);
+    for (int i = 0; i < book->size; ++i) {
+        printf("name %s, number %s, ptr: %p\n", book->contacts[i].name, book->contacts[i].number, (void*)&book->contacts[i]);
     }
 
-    free(book.contacts);
+    free(book);
 }
-
-//int main(){
-//    DynAddressBook addressBook = dynAddressBook_init();
-//    Contact contact = {"Robin", "0723079232"};
-//    addressBook = DynAddContactPtr(&addressBook, contact);
-//    //addressBook = DynAddContact(addressBook, contact);
-//
-////    Contact contact2 = {"Shan", "0723079333"};
-////    addressBook = DynAddContact(addressBook, contact2);
-////
-//    printf("%p\n", &addressBook.contacts[0]);
-//    Contact contact2 = addressBook.contacts[0];
-//    printf("name: %s\nnumber: %s\n", contact2.name, contact2.number);
-//}
-
-//int main() {
-//    printf("This is your address book:\n");
-//
-//    AddressBook addressBook = init_book();
-//
-//    printf("address book pointer: %p\n", &addressBook);
-//
-//    Contact contact = {"Robin", "0723079232"};
-//    addressBook = AddContact(addressBook, contact);
-//
-//    Contact contact2 = {"Shan", "0723079333"};
-//    addressBook = AddContact(addressBook, contact2);
-//
-//    Contact contact3 = {"Ahmed", "0723079888"};
-//    addressBook = AddContact(addressBook, contact3);
-//
-//    Contact contact4 = {"Billy", "0723079111"};
-//    addressBook = AddContact(addressBook, contact4);
-//
-//    printf("address book pointer: %p\n", &addressBook);
-//
-//    print_all_contacts(addressBook);
-//
-//    return 0;
-//}
